8a7acf0abbc0c87d0b63dcb0963d322b1f43b6c3
Andriy0307 <rechunandriy@gmail.com>
2019-09-09 11:47:52 +0300

    add second method

--- OOP/first_method.cpp
+++ OOP/first_method.cpp
@@ -3,27 +3,38 @@
 
 using namespace std;
 
-FirstMethod::FirstMethod(const int& m_,const int& a_,const int& c_, const int& x01){
+ThreeFirstMethods::ThreeFirstMethods(const int& m_,const int& a_,const int& c_,const int& d_, const int& x01){
     m = m_;
     a = a_;
     c = c_;
+    d = d_;
     x0 = x01;
 }
-void FirstMethod::get(int& m_,int& a_,int& c_, int& x01){
+void ThreeFirstMethods::get(int& m_,int& a_,int& c_,int& d_, int& x01){
     m = m_;
     a = a_;
     c = c_;
+    d = d_;
     x0 = x01;
 }
 
-void FirstMethod::random(){
+void ThreeFirstMethods::random1(){
     int t;
     for(int i = 1; i < 10;++i){
-        t = (a*x0 + c)%m;
+        t = (a*x0 + c) % m;
         number.push_back(t);
         x0 = t;
     }
 }
+
+void ThreeFirstMethods::random2(){
+    int t;
+    for(int i=1; i<10; ++i){
+        t = (d*x0*x0 + a*x0 + c) % m;
+    }
+
+}
+
 void FirstMethod::print(){
     for(int i = 0; i < number.size();++i){
         cout<<number[i]<<" ";
